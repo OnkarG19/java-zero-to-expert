c255d333bdc440daabd6f0f69aef69cbcd1706c9
OnkarG19 <itsonkargupta@gmail.com>
2025-08-08 02:27:20 +0530

    Initial commit

---
+++ SumNumbers.java
@@ -0,0 +1,7 @@
+public class SumNumbers {
+    
+    public static void main(String[] args) {
+        int a = 5, b = 7;
+        System.out.println(a + b);
+    }
+}
---
+++ SwapVariables.java
@@ -0,0 +1,9 @@
+public class SwapVariables {
+    public static void main(String[] args) {
+        int x = 10, y = 20;
+        x = x + y;
+        y = x - y;
+        x = x - y;
+        System.out.println("x: " + x + ", y: " + y);
+    }
+}
