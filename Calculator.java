22124e9b288d18164a1613de7ca10bb8a59a7d43
OnkarG19 <itsonkargupta@gmail.com>
2025-08-08 02:32:31 +0530

    New Commit

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 15, b = 3;
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Subtract: " + (a - b));
+        System.out.println("Multiply: " + (a * b));
+        System.out.println("Divide: " + (a / b));
+    }
+}
